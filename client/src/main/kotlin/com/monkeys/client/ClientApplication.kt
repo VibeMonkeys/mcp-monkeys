e7665208b69dce620753c12d200e377133b4da75
stark <wlsdks12@naver.com>
2025-08-19 09:04:10 +0900

    :sparkles: first init (mcp client with server)

---
+++ client/src/main/kotlin/com/monkeys/client/ClientApplication.kt
@@ -0,0 +1,11 @@
+package com.monkeys.client
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClientApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClientApplication>(*args)
+}
---
+++ server/src/test/kotlin/com/monkeys/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.monkeys.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
